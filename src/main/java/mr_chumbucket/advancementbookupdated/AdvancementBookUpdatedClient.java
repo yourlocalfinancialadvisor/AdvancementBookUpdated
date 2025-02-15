aad330ac492a4da7d8eb35c1d7abe2eebf61d92e
Identity Theft <liam.fitz1920@gmail.com>
2025-02-16 03:07:15 +1100

    programmer art resource pack

--- src/main/java/mr_chumbucket/advancementbookupdated/AdvancementBookUpdatedClient.java
+++ src/main/java/mr_chumbucket/advancementbookupdated/AdvancementBookUpdatedClient.java
@@ -3,14 +3,21 @@ package mr_chumbucket.advancementbookupdated;
 import mr_chumbucket.advancementbookupdated.block.entity.ModBlockEntityTypes;
 import mr_chumbucket.advancementbookupdated.renderer.AdvancementBookStandRenderer;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactories;
+import net.minecraft.util.Identifier;
 
-@Environment(EnvType.CLIENT)
 public class AdvancementBookUpdatedClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
+		// Block entity renderer
 		BlockEntityRendererFactories.register(ModBlockEntityTypes.ADVANCEMENT_BOOKSTAND, AdvancementBookStandRenderer::new);
+
+		// Programmer art resource pack
+		FabricLoader.getInstance().getModContainer(AdvancementBookUpdated.MOD_ID).ifPresent(modContainer -> {
+			ResourceManagerHelper.registerBuiltinResourcePack(Identifier.of(AdvancementBookUpdated.MOD_ID, "advancement_book_programmer_art"), modContainer, ResourcePackActivationType.NORMAL);
+		});
 	}
 }
