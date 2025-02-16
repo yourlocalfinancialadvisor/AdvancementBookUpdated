34bf59e7cad85071d3d8fb9adb17cd9fd928ad76
Identity Theft <liam.fitz1920@gmail.com>
2025-02-16 14:30:04 +1100

    tweaked textures

--- src/main/java/mr_chumbucket/advancementbookupdated/AdvancementBookUpdatedConfig.java
+++ src/main/java/mr_chumbucket/advancementbookupdated/AdvancementBookUpdatedConfig.java
@@ -3,6 +3,6 @@ package mr_chumbucket.advancementbookupdated;
 import eu.midnightdust.lib.config.MidnightConfig;
 
 public class AdvancementBookUpdatedConfig extends MidnightConfig {
-    @Entry public static boolean GIVE_FIRST_JOIN = true;
+    @Entry public static boolean GIVE_FIRST_JOIN = false;
     @Entry public static boolean DISABLE_ADVANCEMENT_BUTTON = true;
 }
